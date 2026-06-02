cea431a8607362cb9f04be558dac656d3378605a
Raditdwipratomo <raditdwi478@gmail.com>
2026-06-03 00:03:36 +0700

    finished testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import Hero from "./section/Hero";
 import Navbar from "./section/Navbar";
 import About from "./section/About";
+import Projects from "./section/Projects";
+import Experiences from "./section/Experiences";
+import Testimonial from "./section/Testimonial";
 
 const App = () => {
   return (
@@ -8,6 +11,9 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
+      <Experiences />
+      <Testimonial />
       <section className="min-h-screen" />
       <section className="min-h-screen" />
       <section className="min-h-screen" />
